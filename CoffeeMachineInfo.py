8a78b65702ca7bceb676fd47f41f58b4d19e91ab
mjl1999 <40575814+mjl1999@users.noreply.github.com>
2024-02-13 22:31:27 +0000

    Add files via upload

---
+++ CoffeeMachineInfo.py
@@ -0,0 +1,9 @@
+coffee_options = ["espresso", "latte", "cappuccino", "report", "done"]
+
+coffee_req = [
+    {"name": "espresso", "water": 50, "coffee": 18, "milk": 0},
+    {"name": "latte", "water": 200, "coffee": 24, "milk": 150},
+    {"name": "cappuccino", "water": 250, "coffee": 24, "milk": 100}
+]
+
+resources = {"water": 1000, "milk": 1000, "coffee": 1000, "Money": 0}
